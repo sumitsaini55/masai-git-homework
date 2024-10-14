b898068182d035ff6bf37156db57a7ea792f8f97
sumitsaini55 <sumitsaini1968@gmail.com>
2024-10-14 21:17:34 +0530

    add new code

---
+++ Q12_S5.js
@@ -0,0 +1,5 @@
+let to_do=["take bath","go for meditation","brackfast time","go to office","come back and take dinner"]
+to_do.unshift("go to gym")
+console.log(to_do);
+to_do.pop()
+console.log(to_do);
